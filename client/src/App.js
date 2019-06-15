59966cf8d4ccce220cea765a9b081b92f5e2e37d
Jason Barr <jason@jasonsbarr.com>
2019-06-15 06:05:51 -0500

    Add props to constructor to avoid TS warning

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import { Route } from "react-router-dom";
+import React, { Component } from 'react';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 
 export default class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      savedList: [],
+      savedList: []
     };
   }
 
