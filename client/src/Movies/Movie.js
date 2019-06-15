ba931db3061fbca41e1d4e360bbad02bd701640d
Jason Barr <jason@jasonsbarr.com>
2019-06-15 06:12:55 -0500

    Un-comment functions for stretch goals

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import MovieCard from "./MovieCard";
 
 export default class Movie extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movie: null
+      movie: null,
     };
   }
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const id = Number(this.props.match.params.id);
     this.fetchMovie(id);
   }
 
@@ -26,28 +27,28 @@ export default class Movie extends Component {
       });
   };
   // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
+  componentWillReceiveProps(newProps) {
+    if (this.props.match.params.id !== newProps.match.params.id) {
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie);
+  };
 
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
 
-    const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
         <MovieCard {...this.state.movie} />
+        <div onClick={this.saveMovie} className="save-button">
+          Save
         </div>
-        <div className="save-button">Save</div>
       </div>
     );
   }
