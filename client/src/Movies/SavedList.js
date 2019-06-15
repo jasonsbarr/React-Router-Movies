b34fdb1bf2e5e488142769e59b39b0568cbe06a3
Jason Barr <jason@jasonsbarr.com>
2019-06-15 06:09:22 -0500

    Link Home button to home page

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -10,11 +10,11 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie">
-            <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
-          </span>
+          <span className="saved-movie">{movie.title}</span>
         ))}
+        <Link to="/">
         <div className="home-button">Home</div>
+        </Link>
       </div>
     );
   }
