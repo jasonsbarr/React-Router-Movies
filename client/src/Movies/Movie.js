25a50981f65970b6b28d238c9751f28633e41517
Jason Barr <jason@jasonsbarr.com>
2019-10-11 01:00:59 -0500

    Finish MVP
    AKA change a bunch of things for no reason because it wasn't working, then realize the route to /movies/:id was missing the forward slash at the beginning of the path......

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Movie = props => {
-  const [movie, setMovie] = useState(null);
+  const [movie, setMovie] = useState();
 
   useEffect(() => {
     const id = Number(props.match.params.id);
